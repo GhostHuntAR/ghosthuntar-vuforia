463be79067055f044a84dc50d12356bbfe70529c
Emma Bäckström <backstromemma@gmail.com>
2016-12-19 14:19:13 +0100

    Gyro test, not currently working on android

---
+++ Assets/Scripts/Jumpboy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpboy : MonoBehaviour {
+
+	void Update () {
+
+
+		Vector3 dir = Vector3.zero;
+		// dir.x = Input.acceleration.x;
+		dir.y=-Input.acceleration.x;
+		dir.y = Input.acceleration.y;
+		dir.x = Input.acceleration.y;
+
+
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+
+		dir *= Time.deltaTime;
+		transform.Translate(dir * 5);
+
+	}
+}

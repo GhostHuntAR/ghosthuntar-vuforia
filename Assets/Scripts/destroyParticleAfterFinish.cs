41eb37619ca6a7e01bd1f30d0077d4c4411f94b7
Emma Bäckström <backstromemma@gmail.com>
2016-12-21 17:10:40 +0100

    Ghost prefab, sounds, explosion

---
+++ Assets/Scripts/destroyParticleAfterFinish.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyParticleAfterFinish : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, GetComponent<ParticleSystem>().duration); 
+	}
+	
+}
--- Assets/Scripts/touchManager.cs
+++ Assets/Scripts/touchManager.cs
@@ -4,23 +4,34 @@ using System.Collections;
 public class touchManager : MonoBehaviour {
 
 	public GameObject explosion;
+	public AudioClip explosionSoundClip;
+	public float fireRate = 0.25f;
+	
+	AudioSource explosionSound;
+
+	private float nextFire = 0f;
 
 	// Use this for initialization
 	void Start () {
-	
+		explosionSound = GetComponent<AudioSource>();
+		explosionSound.mute = true;
+		//explosionSound.Stop();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-		if(Input.GetMouseButton(0))
+		if(Input.GetMouseButton(0) && Time.time > nextFire)
 		{
+			nextFire = Time.time + fireRate; 
+
 			Vector3 mousePosFar = new Vector3(Input.mousePosition.x,
 											Input.mousePosition.y,
 											Camera.main.farClipPlane);
 			Vector3 mousePosNear = new Vector3(Input.mousePosition.x,
 											Input.mousePosition.y,
 											Camera.main.nearClipPlane);
+			// translates the screen pos into game world positions
 			Vector3 mousePosF = Camera.main.ScreenToWorldPoint(mousePosFar);
 			Vector3 mousePosN = Camera.main.ScreenToWorldPoint(mousePosNear);
 			Debug.DrawRay(mousePosN, mousePosF - mousePosN, Color.green);
@@ -30,11 +41,16 @@ public class touchManager : MonoBehaviour {
 			if (Physics.Raycast(mousePosN, mousePosF - mousePosN, out hit)) 
 			{
 				Debug.Log("You hit someting");
-				//Debug.Log(hit.transform.position);
+				//Debug.Log(hit.transform.gameObject);
+				//Debug.Log(hit.transform);
 
 				Instantiate(explosion, hit.transform.position, Quaternion.identity);
+				//explosionSound.Play();
+				//AudioSource.PlayClipAtPoint(clip, new Vector3(5, 1, 2));
+				AudioSource.PlayClipAtPoint(explosionSoundClip, hit.transform.position);
+
 				Destroy(hit.transform.gameObject);
-				
+
 			}
 		}
 

13d4ac4c266c5ff85edc9ca6a471b75fd9c52d55
Emma Bäckström <backstromemma@gmail.com>
2016-12-22 11:41:52 +0100

    Updated ghost prefab with moveTowards script

---
+++ Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour {
+	private Vector3 cameraPosition; 
+	private Vector3 ghostPosition;
+	// Use this for initialization
+	void Start () {
+		cameraPosition = GameObject.Find ("ARCamera").transform.position;
+		ghostPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		ghostPosition = Vector3.MoveTowards(ghostPosition, cameraPosition, 0.01f);
+		transform.position = ghostPosition;
+
+		if (ghostPosition == cameraPosition) {
+			Destroy (gameObject);
+			//Player takes damage
+		}
+
+	}
+}
+
+
